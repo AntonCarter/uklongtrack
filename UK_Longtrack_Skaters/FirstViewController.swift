d31034ba905c6880e521ca0d7702450f1cbcb5ea
Anton Carter <acnet@me.com>
2016-03-15 19:12:20 +0000

    Initial Commit

---
+++ UK_Longtrack_Skaters/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  UK_Longtrack_Skaters
+//
+//  Created by Anton Carter on 15/03/2016.
+//  Copyright © 2016 ___ANTONCARTER___. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ UK_Longtrack_Skaters/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  UK_Longtrack_Skaters
+//
+//  Created by Anton Carter on 15/03/2016.
+//  Copyright © 2016 ___ANTONCARTER___. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
